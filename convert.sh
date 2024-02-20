63478a2d2d8b910ae374128756179b3a123e24c0
Hyomin Koo <me@eatsteak.dev>
2024-02-20 13:49:42 +0900

    Initial commit

---
+++ convert.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+echo "Minecraft JE -> BE Model(Geometry) Converter"
+echo "Prerequisites: jq >= 1.6"
+echo "Caution: Cannot convert files that contains multiple textures in one element"
+echo "Awaiting Input...:"
+read file_path

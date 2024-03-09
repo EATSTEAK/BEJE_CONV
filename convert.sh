c9223963f41a500fe3bd40fc9f0126926ec0cbcb
Hyomin Koo <me@eatsteak.dev>
2024-03-09 01:41:47 +0000

    feat: splitting 3d model into single texture model

--- convert.sh
+++ convert.sh
@@ -1,6 +1,21 @@
-#!/bin/zsh
-echo "Minecraft JE -> BE Model(Geometry) Converter"
+#!/bin/bash
+echo "Minecraft JE 3D Model Splitter"
 echo "Prerequisites: jq >= 1.6"
 echo "Caution: Cannot convert files that contains multiple textures in one element"
-echo "Awaiting Input...:"
-read file_path
+file_path="$1"
+file_dir=$(dirname "${1}")
+if [[ -z $1 ]]; then
+  echo ""
+  echo "Usage: ./convert.sh <file>"
+  exit
+fi
+echo "Splitting model into multiple models with single texture..."
+cat $file_path | jq --raw-output '.textures | del(.particle) | keys | .[]' |
+while IFS= read -r line; do
+  output_name=$(cat $file_path | jq --raw-output ".textures.\"$line\"")
+  cat $file_path | jq "\"$line\" as \$raw_texture | \"#\\(\$raw_texture)\" as \$texture |
+.elements |= map(select(.faces | map(.texture | . == \$texture) | all)) |
+.textures |= with_entries(select([.key] | inside([\$raw_texture, \"particle\"]))) |
+.textures |= (.particle = .[\$raw_texture])" > "${file_dir}/${output_name}.json"
+done
+read -r -p "Press Enter to close..."
